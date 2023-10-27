ce34e55fbc6f726e0d500d584ba1b21d9731695c
mohit <mohitrakh01@gmail.com>
2023-10-27 10:53:06 +0530

    bruh

--- api/index.js
+++ api/index.js
@@ -18,8 +18,18 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
 app.listen(3000, () => {
   console.log("server is listening on 3000!!");
+});
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
