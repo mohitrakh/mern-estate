fb5fbaabeccd0439c19f3d2fb4a1279f79bf3add
mohit <mohitrakh01@gmail.com>
2023-10-28 12:01:43 +0530

    update header and make profile make private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import { AiOutlineSearch } from "react-icons/ai";
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 const Header = () => {
+  const { currentUser } = useSelector((store) => store.user);
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl  mx-auto p-3">
@@ -29,8 +30,17 @@ const Header = () => {
           <Link to={"/about"}>
             <li className="hidden sm:inline hover:underline">About</li>
           </Link>
-          <Link to={"/sign-in"}>
-            <li className="hover:underline">Sign In</li>
+
+          <Link to={"/profile"}>
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className="hover:underline">Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
