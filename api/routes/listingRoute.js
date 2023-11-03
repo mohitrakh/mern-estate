bafaff5fc06d3488b1efa651d8ceee04ed1536a5
mohit <mohitrakh01@gmail.com>
2023-11-03 17:18:57 +0530

    complete updated functionality

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -3,6 +3,7 @@ const {
   createListing,
   deleteListing,
   updateListing,
+  getListing,
 } = require("../controllers/listingController");
 const verifyToken = require("../utils/verifyUser");
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.route("/create").post(verifyToken, createListing);
 router.route("/delete/:id").delete(verifyToken, deleteListing);
 router.route("/update/:id").patch(verifyToken, updateListing);
+router.route("/get/:id").get(getListing);
 
 module.exports = router;
