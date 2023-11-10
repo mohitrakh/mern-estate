ac2fd8bafcb09510b3945e13ba73662c21298f21
mohit <mohitrakh01@gmail.com>
2023-11-10 18:51:44 +0530

    search api route

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -4,6 +4,7 @@ const {
   deleteListing,
   updateListing,
   getListing,
+  Listings,
 } = require("../controllers/listingController");
 const verifyToken = require("../utils/verifyUser");
 const router = express.Router();
@@ -12,5 +13,6 @@ router.route("/create").post(verifyToken, createListing);
 router.route("/delete/:id").delete(verifyToken, deleteListing);
 router.route("/update/:id").patch(verifyToken, updateListing);
 router.route("/get/:id").get(getListing);
+router.route("/get").get(Listings);
 
 module.exports = router;
